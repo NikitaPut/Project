76bba042e27509296e59165821d7792452e34e67
Nikita <nikitaputintsev@gmail.com>
2022-09-04 19:17:33 +0300

    first commit

---
+++ Helpers.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include "Helpers.h"
+
+int main() {
+	std::cout << "\t the square of the sum of the numbers 5 and 5: \n" << SumSquared(5, 5) << "\n";
+	std::cout << "\t the square of the sum of the numbers 3.4 and 8.8: \n" << SumSquared(3.4, 8.8) << "\n";
+
+	return 0;
+}
